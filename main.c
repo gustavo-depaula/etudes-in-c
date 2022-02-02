d452e30ce1b825021ea9c4f96d6f9b0a065a1fd5
Gustavo de Paula <gustavodepaula@disroot.org>
2022-02-02 12:14:55 -0300

    heapify

--- main.c
+++ main.c
@@ -146,6 +146,14 @@ size_t get_right_child_index(size_t i) {
 bool has_right_child(generic_array heap, size_t elem_index) {
     return get_right_child_index(elem_index) <= heap.array_size;
 }
+
+void* get_left_child(generic_array heap, size_t elem_index) {
+    return get_unit(heap, get_left_child_index(elem_index));
+}
+void* get_right_child(generic_array heap, size_t elem_index) {
+    return get_unit(heap, get_right_child_index(elem_index));
+}
+
 generic_array make_array_copy(generic_array source) {
     size_t array_size = source.array_size;
     size_t unit_size = source.unit_size;
@@ -177,15 +185,49 @@ void swap_array_positions(generic_array array,
     memcpy(&array.pointer[j], aux, array.unit_size);
 }
 
-void heapify(generic_array heap, size_t index) {
-    size_t j = index;
-    while (2*j < heap.array_size) {
-
+size_t get_top_child_index(generic_array heap, size_t index, bool (*comparator)(void *, void *)) {
+    if (!has_left_child(heap, index)) {
+        return 0;
     }
+
+    void* left_child = get_left_child(heap, index);
+    size_t top_child_index = get_left_child_index(index);
+
+    if (has_right_child(heap, index)) {
+        void* right_child = get_right_child(heap, index);
+        if (comparator(left_child, right_child)) {
+            top_child_index = get_right_child_index(index);
+        }
+    }
+
+    return top_child_index;
 }
 
-generic_array make_heap(generic_array from) {
+void heapify(generic_array heap, size_t index, bool (*comparator)(void *, void *)) {
+    size_t top_child_index = get_top_child_index(heap, index, comparator);
+    if (top_child_index == 0) {
+        return;
+    }
+
+    void* elem = get_unit(heap, index);
+    void* top_child = get_unit(heap, top_child_index);
+    if (comparator(elem, top_child)) {
+        return;
+    }
+
+    swap_array_positions(heap, index, top_child_index);
+    heapify(heap, top_child_index, comparator);
+}
+
+generic_array make_heap(generic_array from, bool (*comparator)(void *, void *)) {
     generic_array heap = make_array_copy(from);
+
+    if (heap.array_size > 0) {
+        size_t i = 0;
+        for (i = heap.array_size; i >= 0; --i) {
+            heapify(heap, i, comparator);
+        }
+    }
 
     return heap;
 }
