7754c2ee8f109efead178122731e491af07761e3
Gustavo de Paula <gustavodepaula@disroot.org>
2022-02-02 16:17:50 -0300

    export print_heap

--- src/heaps.h
+++ src/heaps.h
@@ -12,5 +12,6 @@ typedef struct {
 Heap make_heap(generic_array from, bool (*comparator)(void *, void *));
 void* extract(Heap* heap);
 void insert(Heap* heap, void* unit);
+void print_heap(generic_array heap, size_t index, void (*print)(void *));
 
 #endif // IFNDEF HEAPS_H_
